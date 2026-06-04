483fc31327f8a68d4e8dfb46e40b9bf689005974
Harsha <harshasai1026@gmail.com>
2026-06-04 12:30:39 +0530

    Initial project setup

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,46 @@
+import userModel from "../models/user.model.js";
+import crypt from "crypto";
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+export async function register(req,res){
+    const {username, email, password} = req.body;
+
+    const isAlreadyRegistered = await userModel.findOne({
+        $or: [
+            {usrname},
+            {email}
+        ]
+    })
+
+    if(isAlreadyRegistered){
+        res.status(409).json({
+            message: "Username or email already exists"
+        })
+    }
+
+    const hashedPassword = crypt.createHash("sha256").update(password).digest("hex");
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    const token = jwt.sign({
+        id : user._id
+    }, config.JWT_SECRET, 
+        {
+            expiresIn: "1d"
+        }
+    )
+    
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user: {
+            username: user.username,
+            email: user.email,
+            token
+        }
+    })
+}
